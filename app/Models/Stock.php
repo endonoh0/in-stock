1174fa2bae47f2cc2eb9db353da64fc1d85013ad
Eric Ho <endonoho@gmail.com>
2020-10-08 13:28:31 -0700

    Implement fake HTTP request to fetch product's stock details

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Stock extends Model
 {
@@ -13,4 +14,24 @@ class Stock extends Model
     protected $casts = [
         'in_stock' => 'boolean'
     ];
+
+    public function track()
+    {
+        if ($this->retailer->name === 'Best Buy') {
+            // Hit an API endpoint for the associated retailer
+            // fetch the up-to-date details for the item
+            $results = Http::get('http://foo.test')->json();
+
+            // and then refresh the current stock record
+            $this->update([
+                'in_stock' => $results['available'],
+                'price' => $results['price']
+            ]);
+        }
+    }
+
+    public function retailer()
+    {
+        return $this->belongsTo(Retailer::class);
+    }
 }
