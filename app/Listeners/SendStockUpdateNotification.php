70c0e6c820a372ddb511b5692196485742b391d1
Eric Ho <endonoho@gmail.com>
2020-10-09 15:26:42 -0700

    Add Event Listener

---
+++ app/Listeners/SendStockUpdateNotification.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use App\Events\NowInStock;
+use App\Notifications\StockUpdate;
+
+class SendStockUpdateNotification
+{
+    /**
+     * Handle the event.
+     *
+     * @param  NowInStock  $event
+     * @return void
+     */
+    public function handle(NowInStock $event)
+    {
+        User::first()->notify(new StockUpdate);
+    }
+}
